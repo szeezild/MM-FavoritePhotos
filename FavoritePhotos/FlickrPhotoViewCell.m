d680cc4b58fe935287f5c849a2804e6671331749
Dan Szeezil <szeezild@gmail.com>
2014-04-01 08:49:37 -0500

    initial commit

---
+++ FavoritePhotos/FlickrPhotoViewCell.m
@@ -0,0 +1,31 @@
+//
+//  FlickrPhotoViewCell.m
+//  FavoritePhotos
+//
+//  Created by Dan Szeezil on 3/31/14.
+//  Copyright (c) 2014 Dan Szeezil. All rights reserved.
+//
+
+#import "FlickrPhotoViewCell.h"
+
+@implementation FlickrPhotoViewCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
